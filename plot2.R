5fe2a56106a90b8777d11256ac788f2373b42049
Divya Swaminathan <sdivya.swaminathan@gmail.com>
2016-05-15 16:32:19 -0700

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,15 @@
+# 15 MAY 2016 @ DS
+# Read the data
+df<-read.table(file="household_power_consumption.txt",sep=";",header=TRUE)
+# subset the data
+df_ss<-df[df[,1]=='1/2/2007'| df[,1]=='2/2/2007',] # logic is day month year.
+# convert variable global active power to numeric values 
+GAP<-as.numeric(levels(df_ss$Global_active_power))[df_ss$Global_active_power]
+# use strptime to manipulate date and tim
+datetime<-strptime(paste(df_ss$Date, df_ss$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+# open PNG device and plot histogram
+png('plot2.png',width=480, height = 480)
+plot(datetime,GAP,type='l',xlab="",ylab="Global Active Power (kilowatts)")
+dev.off()
+#
+#
