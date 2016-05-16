0d06a9a49f7a718b99b797e54c5302b2e1838474
Divya Swaminathan <sdivya.swaminathan@gmail.com>
2016-05-15 18:03:34 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -7,7 +7,7 @@ df_ss<-df[df[,1]=='1/2/2007'| df[,1]=='2/2/2007',] # logic is day month year.
 GAP<-as.numeric(levels(df_ss$Global_active_power))[df_ss$Global_active_power]
 # use strptime to manipulate date and time
 datetime<-strptime(paste(df_ss$Date, df_ss$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-# open PNG device and plot histogram
+# open PNG device and plot graph
 png('plot2.png',width=480, height = 480)
 plot(datetime,GAP,type='l',xlab="",ylab="Global Active Power (kilowatts)")
 dev.off()
