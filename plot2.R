af209d7bd5ce2fa26b63dabeb34169414df15bf5
Divya Swaminathan <sdivya.swaminathan@gmail.com>
2016-05-15 18:03:06 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -5,7 +5,7 @@ df<-read.table(file="household_power_consumption.txt",sep=";",header=TRUE)
 df_ss<-df[df[,1]=='1/2/2007'| df[,1]=='2/2/2007',] # logic is day month year.
 # convert variable global active power to numeric values 
 GAP<-as.numeric(levels(df_ss$Global_active_power))[df_ss$Global_active_power]
-# use strptime to manipulate date and tim
+# use strptime to manipulate date and time
 datetime<-strptime(paste(df_ss$Date, df_ss$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 # open PNG device and plot histogram
 png('plot2.png',width=480, height = 480)
